a0fedb10e8db708846d28abbf00c838c2c81dd51
luismts <deivi_0326@hotmail.com>
2019-03-03 12:15:48 -0400

    add methods to set the valorations

---
+++ src/module 3/Exercice 1/valorationservice.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.4.19;
+
+import "./valorationfactory.sol";
+import "./productfactory.sol";
+
+contract ValorationService is ValorationFactory, ProductFactory {
+
+  modifier onlyOwnerOf(uint _productId) {
+    require(msg.sender == productToOwner[_productId], "Owner not allowed");
+    _;
+  }
+
+  function setValoration(
+    address _owner, uint _productId, uint _delivery, uint _product, 
+    uint _qualityprice, bytes32 _comment) internal onlyOwnerOf(_productId) {
+    
+    _createValoration(_owner, _delivery, _product, _qualityprice, _comment);
+  }
+
+  function setGeneralValoration(address _owner, uint _generalValoration, uint _productId) internal onlyOwnerOf(_productId) {
+    
+    createGeneralValoration(_owner, _generalValoration);
+  }
+  
+}
