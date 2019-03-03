c0dbdb7d72e55e156bc9fed5c3eff3982dd79cbb
luismts <deivi_0326@hotmail.com>
2019-03-03 09:26:03 -0400

    adding owner to the logic

--- src/module 3/Exercice 1/valorationfactory.sol
+++ src/module 3/Exercice 1/valorationfactory.sol
@@ -4,36 +4,36 @@ import "./ownable.sol";
 
 contract ValorationFactory is Ownable {
 
-  event NewValoration(uint valorationId, uint delivery, uint product, uint qualityprice, bytes32 comment);
+  event NewValoration(uint valorationId, uint delivery, uint productId, uint product, uint qualityprice, bytes32 comment);
 
   struct Valoration {
     uint id;
     uint delivery;
+    uint productId;
     uint product;
     uint qualityprice;
     bytes32 comment;
   }
 
   Valoration[] public valorations;
-  uint nonce = 0;
 
-  mapping (uint => address) public valorationToProduct;
+  mapping (uint => address) public valorationToOwner;
   mapping (address => uint) ownerValorationCount;
 
-  function _createValoration(uint delivery, uint product, uint qualityprice, bytes32 comment) internal {
+  function _createValoration(address owner, uint delivery, uint productId, uint product, uint qualityprice, bytes32 comment) internal {
    
     uint valorationId = valorations.length + 1;
-    uint id = valorations.push(Valoration(valorationId, delivery, product, qualityprice, comment)) - 1;
+    uint id = valorations.push(Valoration(valorationId, delivery, productId, product, qualityprice, comment)) - 1;
 
-    valorationToProduct[id] = msg.sender;
-    ownerValorationCount[msg.sender]++;
+    valorationToOwner[id] = owner;
+    ownerValorationCount[owner]++;
 
-    emit NewValoration(id, delivery, product, qualityprice, comment);
+    emit NewValoration(id, delivery, productId, product, qualityprice, comment);
   }
 
-  function createGeneralValoration(uint _generalValoration) public {
+  function createGeneralValoration(address _owner, uint _generalValoration, uint productId) public {
     require(ownerValorationCount[msg.sender] == 0, "Valoration not found");    
-    _createValoration(_generalValoration, _generalValoration, _generalValoration, "No comments");
+    _createValoration(_owner, _generalValoration, productId, _generalValoration, _generalValoration, "No comments");
   }
 
 }
