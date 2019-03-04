aacc998658aa14f9bb8802ead3140559743542de
luismts <deivi_0326@hotmail.com>
2019-03-03 22:07:59 -0400

    new event added

--- src/module 3/Exercice 1/productsale.sol
+++ src/module 3/Exercice 1/productsale.sol
@@ -4,9 +4,13 @@ import "./producthelper.sol";
 
 contract ProductSale is ProductHelper {
 
+  event NewSale(uint productId, uint quantity, address _buyer);
+
   function sale(uint _productId, address _buyer, uint quantity) external onlyOwnerOf(_productId) {
 
     saleAndDecrease(_productId, quantity);
     byerProducts[_buyer].push(_productId);
+
+    emit NewSale(_productId, quantity, _buyer);
   }
 }
